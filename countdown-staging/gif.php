feac4d7e4580005449dfbd98bc463ae948a878b0
Natan Siqueira <natan.siqueira@belezanaweb.com.br>
2018-10-17 12:22:42 -0300

    Initial commit

---
+++ countdown-staging/gif.php
@@ -0,0 +1,61 @@
+<?php
+	//Leave all this stuff as it is
+	date_default_timezone_set('America/Sao_Paulo;');
+	include 'GIFEncoder.class.php';
+	include 'php52-fix.php';
+	$time = $_GET['time'];
+	$future_date = new DateTime(date('r',strtotime($time)));
+	$time_now = time();
+	$now = new DateTime(date('r', $time_now));
+	$frames = array();	
+	$delays = array();
+
+	$image = imagecreatefrompng('images/countdown.png');
+
+	$delay = 100;
+
+	$font = array(
+		'size' => 23,
+		'angle' => 0,
+		'x-offset' => 7, 
+		'y-offset' => 30,
+		'file' => __DIR__ . DIRECTORY_SEPARATOR . 'Futura.ttc',
+		'color' => imagecolorallocate($image, 55, 160, 130)
+	);
+
+	for($i = 0; $i <= 60; $i++){
+		
+		$interval = date_diff($future_date, $now);
+		
+		if($future_date < $now){
+			$text = $interval->format('00:00:00:00');
+			imagettftext ($image , $font['size'] , $font['angle'] , $font['x-offset'] , $font['y-offset'] , $font['color'] , $font['file'], $text );
+			ob_start();
+			imagegif($image);
+			$frames[]=ob_get_contents();
+			$delays[]=$delay;
+			$loops = 1;
+			ob_end_clean();
+			break;
+		} else {
+			;
+			$text = $interval->format('0%a %H %I %S');
+			imagettftext ($image , $font['size'] , $font['angle'] , $font['x-offset'] , $font['y-offset'] , $font['color'] , $font['file'], $text );
+			ob_start();
+			imagegif($image);
+			$frames[]=ob_get_contents();
+			$delays[]=$delay;
+			$loops = 0;
+			ob_end_clean();
+		}
+
+		$now->modify('+1 second');
+	}
+
+	header( 'Expires: Sat, 26 Jul 1997 05:00:00 GMT' );
+	header( 'Last-Modified: ' . gmdate( 'D, d M Y H:i:s' ) . ' GMT' );
+	header( 'Cache-Control: no-store, no-cache, must-revalidate' );
+	header( 'Cache-Control: post-check=0, pre-check=0', false );
+	header( 'Pragma: no-cache' );
+	$gif = new AnimatedGif($frames,$delays,$loops);
+	$gif->display();
