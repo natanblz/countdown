e50c4cf4aaf6b9dabf45f6fda3f502628d05dc9f
Natan Siqueira <natan.siqueira@belezanaweb.com.br>
2018-10-17 16:52:02 -0300

    change font

--- countdown-staging/gif.php
+++ countdown-staging/gif.php
@@ -1,5 +1,4 @@
 <?php
-	//Leave all this stuff as it is
 	date_default_timezone_set('America/Sao_Paulo');
 	include '../commons/GIFEncoder.class.php';
 	include '../commons/php52-fix.php';
@@ -10,40 +9,47 @@
 	$frames = array();	
 	$delays = array();
 
-	// Your image link
 	$image = imagecreatefrompng('images/countdown.png');
 	$delay = 100;// milliseconds
 	$font = array(
-		'size' => 17, // Font size, in pts usually.
+		'size' => 60, // Font size, in pts usually.
 		'angle' => 0, // Angle of the text
-		'x-offset' => 7, // The larger the number the further the distance from the left hand side, 0 to align to the left.
-		'y-offset' => 30, // The vertical alignment, trial and error between 20 and 60.
+		'x-offset' => 40, // The larger the number the further the distance from the left hand side, 0 to align to the left.
+		'y-offset' => 75, // The vertical alignment, trial and error between 20 and 60.
 		'file' => __DIR__ . DIRECTORY_SEPARATOR . 'HelveticaNeue.otf', // Font path
-		'color' => imagecolorallocate($image, 55, 160, 130), // RGB Colour of the text
+		'color' => imagecolorallocate($image, 255, 108, 0), // RGB Colour of the text
 	);
+
 	for($i = 0; $i < 60; $i++){
 		
 		$interval = date_diff($future_date, $now);
 		
 		if($future_date < $now){
-			// Open the first source image and add the text.
 			$image = imagecreatefrompng('images/countdown.png');
-			$text = $interval->format('00:00:00:00');
+
+			$text = $interval->format('00 : 00 : 00 : 00');
+
 			imagettftext ($image , $font['size'] , $font['angle'] , $font['x-offset'] , $font['y-offset'] , $font['color'] , $font['file'], $text );
 			ob_start();
+
 			imagegif($image);
+
 			$frames[]=ob_get_contents();
 			$delays[]=$delay;
 			$loops = 1;
+
 			ob_end_clean();
 			break;
 		} else {
-			// Open the first source image and add the text.
 			$image = imagecreatefrompng('images/countdown.png');
-			$text = $interval->format('0%a %H %I %S');
+
+			$text = $interval->format('%a : %H : %I : %S');
+
 			imagettftext ($image , $font['size'] , $font['angle'] , $font['x-offset'] , $font['y-offset'] , $font['color'] , $font['file'], $text );
 			ob_start();
+
 			imagegif($image);
+
 			$frames[]=ob_get_contents();
 			$delays[]=$delay;
 			$loops = 0;
@@ -51,7 +57,7 @@
 		}
 		$now->modify('+1 second');
 	}
-	//expire this image instantly
+
 	header( 'Expires: Sat, 26 Jul 1997 05:00:00 GMT' );
 	header( 'Last-Modified: ' . gmdate( 'D, d M Y H:i:s' ) . ' GMT' );
 	header( 'Cache-Control: no-store, no-cache, must-revalidate' );
