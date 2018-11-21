dea3453d56774b6383c2dbdd2df360836709cdc2
Natan Siqueira <natan.siqueira@belezanaweb.com.br>
2018-11-21 17:47:40 -0200

    Add bf countdown

--- countdown-blackfriday/gif.php
+++ countdown-blackfriday/gif.php
@@ -43,7 +43,7 @@
 
 			$text = $interval->format('0%a : %H : %I : %S');
 
-			imagettftext ($image , $font['size'] , $font['angle'] , 20 , $font['y-offset'] , $font['color'] , $font['file'], $text );
+			imagettftext ($image , 40 , $font['angle'] , 20 , $font['y-offset'] , $font['color'] , $font['file'], $text );
 			ob_start();
 
 			imagegif($image);
